4ab4025f7659dffbe7b0dc0e5133502afb4674c0
David Cullen <dcullen@jungroup.com>
2014-08-25 18:00:23 -0400

    Analytics Initial

---
+++ app/controllers/AnalyticsController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+import anorm._
+import play.api.db.DB
+import models._
+import play.api.libs.json._
+
+object AnalyticsController extends Controller {
+  def show(distributorID: Long, appID: Long) = Action {
+    val app = App.find(appID)
+    Ok(views.html.Analytics.show(app.get, distributorID, appID))
+  }
+}
+
