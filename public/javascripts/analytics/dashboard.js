5feb82a69fb5aa7544f7c750f4b9d984bb33b194
David Cullen <dcullen@jungroup.com>
2014-09-03 09:48:41 -0400

    Analytics clean up

--- public/javascripts/analytics/dashboard.js
+++ public/javascripts/analytics/dashboard.js
@@ -5,11 +5,6 @@ var client = new Keen({
     readKey: "38e91b786e4c8150f22eac2368b038bc50d7e2a6904e97578a32e11d08a89b1ec1192272df9d9b7ca2586d5852e059f5604c702ded6d914ba68f14e8049d6023b076555e23500a8baf660c503b038a0a3fc9050872441938525c888a65cb49b85186e1b060fa5ceb8256351ef22c0902"
 });
 
-var client_demo = new Keen({
-    projectId: "5368fa5436bf5a5623000000",
-    readKey: "3f324dcb5636316d6865ab0ebbbbc725224c7f8f3e8899c7733439965d6d4a2c7f13bf7765458790bd50ec76b4361687f51cf626314585dc246bb51aeb455c0a1dd6ce77a993d9c953c5fc554d1d3530ca5d17bdc6d1333ef3d8146a990c79435bb2c7d936f259a22647a75407921056"
-});
-
 function isValidDate( date ) {
     if ( isNaN( date.getTime() ) ) {  // d.valueOf() could also work
         return false;
@@ -33,21 +28,82 @@ function updateCharts( event ) {
     var start_date_iso = start_date.toISOString();
     var end_date_iso = end_date.toISOString();
 
-    Keen.ready(function(){
+    Keen.ready( function() {
+        // Inventory Request count, metric
+        var inventory_request = new Keen.Query( "count", {
+            eventCollection: "Inventory Request",
+            filters: [ {
+                property_name: "app",
+                operator: "eq",
+                property_value: 1
+            } ]
+        } );
+        client.draw( inventory_request, document.getElementById( "inventory_requests" ), {
+            chartType: "metric",
+            title: "Requests",
+            colors: [ "#4285f4" ],
+            width: $( "#inventory_requests" ).width()
+        });
 
-        // ----------------------------------------
-        // Pageviews Area Chart
-        // ----------------------------------------
+        // Unique Users count, metric
+        var unique_users = new Keen.Query( "count", {
+            eventCollection: "Inventory Request",
+            filters: [ {
+                property_name: "app",
+                operator: "eq",
+                property_value: 1
+            } ]
+        } );
+        client.draw( unique_users, document.getElementById( "unique_users" ), {
+            chartType: "metric",
+            title: "Unique Users",
+            colors: [ "#4285f4" ],
+            width: $( "#unique_users" ).width()
+        });
+
+        // Fill rate, metric
+        var fill_rate = new Keen.Query( "count", {
+            eventCollection: "Inventory Request",
+            filters: [ {
+                property_name: "app",
+                operator: "eq",
+                property_value: 1
+            } ]
+        } );
+        client.draw( fill_rate, document.getElementById( "fill_rate" ), {
+            chartType: "metric",
+            title: "Fill Rate",
+            colors: [ "#4285f4" ],
+            width: $( "#fill_rate" ).width()
+        });
+
+        // Estimated Revenue, table
+        var estimated_revenue = new Keen.Query( "count", {
+            eventCollection: "Inventory Request",
+            filters: [ {
+                property_name: "app",
+                operator: "eq",
+                property_value: 1
+            } ]
+        } );
+        client.draw( estimated_revenue, document.getElementById( "estimated_revenue" ), {
+            chartType: "metric",
+            title: "Estimated Revenue",
+            colors: [ "#4285f4" ],
+            width: $( "#estimated_revenue" ).width()
+        });
+
+        // Estimated Revenue Chart
         var pageviews_timeline = new Keen.Query("count", {
             eventCollection: "pageviews",
             interval: "hourly",
             groupBy: "user.device_info.browser.family",
             timeframe: {
-                start: "2014-05-04T00:00:00.000Z",
-                end: "2014-05-05T00:00:00.000Z"
+                start: start_date_iso,
+                end: end_date_iso
             }
         });
-        client_demo.draw(pageviews_timeline, document.getElementById("chart-01"), {
+        client.draw(pageviews_timeline, document.getElementById("estimated_revenue_chart"), {
             chartType: "areachart",
             title: false,
             height: 250,
@@ -62,179 +118,6 @@ function updateCharts( event ) {
                 isStacked: true
             }
         });
-
-
-        // ----------------------------------------
-        // Pageviews Pie Chart
-        // ----------------------------------------
-        var pageviews_static = new Keen.Query("count", {
-            eventCollection: "pageviews",
-            groupBy: "user.device_info.browser.family",
-            timeframe: {
-                start: "2014-05-04T00:00:00.000Z",
-                end: "2014-05-05T00:00:00.000Z"
-            }
-        });
-        client_demo.draw(pageviews_static, document.getElementById("chart-02"), {
-            chartType: "piechart",
-            title: false,
-            height: 250,
-            width: "auto",
-            chartOptions: {
-                chartArea: {
-                    height: "85%",
-                    left: "5%",
-                    top: "5%",
-                    width: "100%"
-                }
-            }
-        });
-
-
-        // ----------------------------------------
-        // Impressions timeline
-        // ----------------------------------------
-        var impressions_timeline = new Keen.Query("count", {
-            eventCollection: "impressions",
-            groupBy: "ad.advertiser",
-            interval: "hourly",
-            timeframe: {
-                start: "2014-05-04T00:00:00.000Z",
-                end: "2014-05-05T00:00:00.000Z"
-            }
-        });
-        client_demo.draw(impressions_timeline, document.getElementById("chart-03"), {
-            chartType: "columnchart",
-            title: false,
-            height: 250,
-            width: "auto",
-            chartOptions: {
-                chartArea: {
-                    height: "75%",
-                    left: "10%",
-                    top: "5%",
-                    width: "60%"
-                },
-                bar: {
-                    groupWidth: "85%"
-                },
-                isStacked: true
-            }
-        });
-
-
-        // ----------------------------------------
-        // Impressions timeline (device)
-        // ----------------------------------------
-        var impressions_timeline_by_device = new Keen.Query("count", {
-            eventCollection: "impressions",
-            groupBy: "user.device_info.device.family",
-            interval: "hourly",
-            timeframe: {
-                start: "2014-05-04T00:00:00.000Z",
-                end: "2014-05-05T00:00:00.000Z"
-            }
-        });
-        client_demo.draw(impressions_timeline_by_device, document.getElementById("chart-04"), {
-            chartType: "columnchart",
-            title: false,
-            height: 250,
-            width: "auto",
-            chartOptions: {
-                chartArea: {
-                    height: "75%",
-                    left: "10%",
-                    top: "5%",
-                    width: "60%"
-                },
-                bar: {
-                    groupWidth: "85%"
-                },
-                isStacked: true
-            }
-        });
-
-
-        // ----------------------------------------
-        // Impressions timeline (country)
-        // ----------------------------------------
-        var impressions_timeline_by_country = new Keen.Query("count", {
-            eventCollection: "impressions",
-            groupBy: "user.geo_info.country",
-            interval: "hourly",
-            timeframe: {
-                start: "2014-05-04T00:00:00.000Z",
-                end: "2014-05-05T00:00:00.000Z"
-            }
-        });
-        client_demo.draw(impressions_timeline_by_country, document.getElementById("chart-05"), {
-            chartType: "columnchart",
-            title: false,
-            height: 250,
-            width: "auto",
-            chartOptions: {
-                chartArea: {
-                    height: "75%",
-                    left: "10%",
-                    top: "5%",
-                    width: "60%"
-                },
-                bar: {
-                    groupWidth: "85%"
-                },
-                isStacked: true
-            }
-        });
-
-        var inventory_requests_per_day = new Keen.Query("count", {
-            eventCollection: "Inventory Request",
-            filters: [{"property_name":"distributor_id","operator":"eq","property_value":1}],
-            groupBy: "app",
-            timeframe: {
-                start: start_date_iso,
-                end: end_date_iso
-            }
-        });
-
-        client.draw(inventory_requests_per_day, document.getElementById("table_by_day"), {
-            chartType: "table",
-            title: "Per Day",
-            width: $("#table_by_day").width(),
-            cssClassNames: {
-                headerRow: "dashboard_table_header_row"
-            }
-        });
-
-        var query = new Keen.Query("count", {
-            eventCollection: "Inventory Request",
-            filters: [{"property_name":"app","operator":"eq","property_value":1},
-                {"property_name":"distributor_id","operator":"eq","property_value":1}]
-        });
-        client.draw(query, document.getElementById("inventory_requests"), {
-            chartType: "metric",
-            title: "Requests",
-            colors: ["#4285f4"],
-            width: $("#inventory_requests").width()
-        });
-        client.draw(query, document.getElementById("unique_users"), {
-            chartType: "metric",
-            title: "Unique Users",
-            colors: ["#4285f4"],
-            width: $("#unique_users").width()
-        });
-        client.draw(query, document.getElementById("fill_rate"), {
-            chartType: "metric",
-            title: "Fill Rate",
-            colors: ["#4285f4"],
-            width: $("#fill_rate").width()
-        });
-        client.draw(query, document.getElementById("estimated_revenue"), {
-            chartType: "metric",
-            title: "Estimated Revenue",
-            colors: ["#4285f4"],
-            width: $("#estimated_revenue").width()
-        });
-
     });
 };
 
