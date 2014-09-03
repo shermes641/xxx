2c9df1212ce472fb65bc119ea4ea223962e3454e
David Cullen <dcullen@jungroup.com>
2014-09-03 13:23:17 -0400

    Keen IO dependency configuration

--- build.sbt
+++ build.sbt
@@ -13,5 +13,8 @@ libraryDependencies ++= Seq(
   ws,
   "org.postgresql" % "postgresql" % "9.3-1100-jdbc4",
   "com.typesafe.play.plugins" %% "play-plugins-mailer" % "2.3.0",
-  "com.github.t3hnar" %% "scala-bcrypt" % "2.4"
+  "com.github.t3hnar" %% "scala-bcrypt" % "2.4",
+  "org.clapper" %% "grizzled-slf4j" % "1.0.2",
+  "io.spray" %%  "spray-can"     % "1.3.1",
+  "io.spray" %%  "spray-routing" % "1.3.1"
 )
--- lib/keenImport.scala
+++ lib/keenImport.scala
@@ -1,29 +1,29 @@
 import io.keen.client.scala.Client
 
-//val client = new Client(
-//  projectId = sys.env("KEEN_PROJECT_ID"),
-//  masterKey = sys.env.get("KEEN_MASTER_KEY"),
-//  writeKey = sys.env.get("KEEN_WRITE_KEY"),
-//  readKey = sys.env.get("KEEN_READ_KEY")
-//)
-//
-//
-//// Publish an event!
-//client.addEvent(
-//  collection = "collectionNameHere",
-//  event = """{"foo": "bar"}"""
-//)
-//
-//// Publish an event and care about the result!
-//val resp = client.addEvent(
-//  collection = "collectionNameHere",
-//  event = """{"foo": "bar"}"""
-//)
+val client = new Client(
+  projectId = "53f75d42709a3952e3000002",
+  masterKey = Option("69FD3FC4F9C6366602DE0E230331D3B8"),
+  writeKey = Option("5594a568ccf77b8c4d389a60daf4e99b9a65b2e39b9a5bc9faeda098bc410121cc3dafe5b957bdd9f58dcfbb41d0d27908507c7fa6dc705399fec29e4c867b52df2666c34c0558e3d52806d0e14e81d7bee9e8d43f571f0c1452ca05ccf66f54a78d06764d3cba08e6cbfd2e897ec20c"),
+  readKey = Option("38e91b786e4c8150f22eac2368b038bc50d7e2a6904e97578a32e11d08a89b1ec1192272df9d9b7ca2586d5852e059f5604c702ded6d914ba68f14e8049d6023b076555e23500a8baf660c503b038a0a3fc9050872441938525c888a65cb49b85186e1b060fa5ceb8256351ef22c0902")
+)
+
+
+// Publish an event!
+client.addEvent(
+  collection = "collectionNameHere",
+  event = """{"foo": "bar"}"""
+)
+
+// Publish an event and care about the result!
+val resp = client.addEvent(
+  collection = "collectionNameHere",
+  event = """{"foo": "bar"}"""
+)
 //
 //// Publish lots of events
 //client.addEvents(someEvents)
-//
-//// Add an onComplete callback for failures!
+
+// Add an onComplete callback for failures!
 //resp onComplete {
 //  case Success(r) => println(resp.statusCode)
 //  case Failure(t) => println(t.getMessage) // A Throwable
