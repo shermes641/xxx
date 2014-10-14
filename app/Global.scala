087c6366959dfe682fedf05d057a33f292e24ffb
Tom Depplito <tdepplito@jungroup.com>
2014-10-14 08:26:15 -0400

    Added check for controller route in HTTPAuthFilter

--- app/Global.scala
+++ app/Global.scala
@@ -6,22 +6,29 @@ import play.api.mvc.Results.Unauthorized
 // Filter to secure staging server. We should remove this before Mediation goes live.
 object HTTPAuthFilter extends Filter {
   def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
-    if(play.api.Play.isProd(play.api.Play.current)) {
-      val httpAuthUser = Play.current.configuration.getString("httpAuthUser").get
-      val httpAuthPassword = Play.current.configuration.getString("httpAuthPassword").get
+    val isStaging = Play.current.configuration.getString("staging") match {
+      case Some(_: String) if(play.api.Play.isProd(play.api.Play.current)) => true
+      case None => false
+    }
+    request.tags.get("ROUTE_CONTROLLER") match {
+      case Some(controller: String) if(controller != "controllers.APIController" && isStaging) => {
+        val httpAuthUser = Play.current.configuration.getString("httpAuthUser").get
+        val httpAuthPassword = Play.current.configuration.getString("httpAuthPassword").get
 
-      request.headers.get("Authorization").flatMap { authorization =>
-        authorization.split(" ").drop(1).headOption.filter { encoded =>
-          new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
-            case user :: password :: Nil if user == httpAuthUser && password == httpAuthPassword => true
-            case _ => false
-          }
-        }.map(_ => next(request))
-      }.getOrElse {
-        Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
+        request.headers.get("Authorization").flatMap { authorization =>
+          authorization.split(" ").drop(1).headOption.filter { encoded =>
+            new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
+              case user :: password :: Nil if user == httpAuthUser && password == httpAuthPassword => true
+              case _ => false
+            }
+          }.map(_ => next(request))
+        }.getOrElse {
+          Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured""""))
+        }
       }
-    } else {
-      next(request)
+      case _ => {
+        next(request)
+      }
     }
   }
 }
