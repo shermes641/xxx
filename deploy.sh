7399b05d404411e459a7d6ea3c088a69a6ae098e
Tom Depplito <tdepplito@jungroup.com>
2014-12-01 13:57:50 -0500

    [MED-278] Modified deploy success check in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
+update_commit_info () {
+    if [ $? -eq 1 ]; then
+        echo Deploy Failed.  SHA and Branch name were not updated.
+    else
+        hash_name=LATEST_SHA
+        hash=$(git rev-parse HEAD)
+        branch_name=LATEST_BRANCH
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        echo Setting $hash_name to $hash
+        echo Setting $branch_name to $branch
+        heroku config:set $hash_name=$hash
+        heroku config:set $branch_name=$branch
+    fi
+}
+
 if [ -z "$1" ]; then
     echo Pushing master branch to Heroku
-    result=$(git push heroku master)
+    update_commit_info $(git push heroku master)
 else
     echo Pushing branch: $1 to Heroku
-    result=$(git push heroku $1:master)
-fi
-
-error_condition=.*error\:\ failed\ to\ push*.
-echo The result of the deploy is as follows: $result
-if [[ $result =~ $error_condition ]]; then
-    echo Deploy Failed.  SHA and Branch name were not updated.
-else
-    hash_name=LATEST_SHA
-    hash=$(git rev-parse HEAD)
-    branch_name=LATEST_BRANCH
-    branch=$(git rev-parse --abbrev-ref HEAD)
-    echo Setting $hash_name to $hash
-    echo Setting $branch_name to $branch
-    heroku config:set $hash_name=$hash
-    heroku config:set $branch_name=$branch
+    update_commit_info $(git push heroku $1:master)
 fi
