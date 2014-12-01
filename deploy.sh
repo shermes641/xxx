a5bfcd10a32163e917ca7ea501d4d1c80d733df2
Tom Depplito <tdepplito@jungroup.com>
2014-12-01 12:38:00 -0500

    [MED-278] Modified shell script to not update SHA and Branch name if deploy fails

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
-hash_name=LATEST_SHA
-hash=$(git rev-parse HEAD)
-branch_name=LATEST_BRANCH
-branch=$(git rev-parse --abbrev-ref HEAD)
-echo Setting $hash_name to $hash
-echo Setting $branch_name to $branch
-heroku config:set $hash_name=$hash
-heroku config:set $branch_name=$branch
 if [ -z "$1" ]; then
     echo Pushing master branch to Heroku
-    git push heroku master
+    result=$(git push heroku master)
 else
     echo Pushing branch: $1 to Heroku
-    git push heroku $1:master
+    result=$(git push heroku $1:master)
+fi
+
+error_condition=.*error\:\ failed\ to\ push*.
+if [[ $result =~ $error_condition ]]; then
+    echo Deploy Failed.  SHA and Branch name were not updated.
+else
+    hash_name=LATEST_SHA
+    hash=$(git rev-parse HEAD)
+    branch_name=LATEST_BRANCH
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    echo Setting $hash_name to $hash
+    echo Setting $branch_name to $branch
+    heroku config:set $hash_name=$hash
+    heroku config:set $branch_name=$branch
 fi
