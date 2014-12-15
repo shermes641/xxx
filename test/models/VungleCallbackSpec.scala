074fb6bee3c568fe72d5a5d78cbfce8809ccdb3f
Tom Depplito <tdepplito@jungroup.com>
2014-12-15 13:56:20 -0500

    [MED-359] Added tests for VungleCallback model

---
+++ test/models/VungleCallbackSpec.scala
@@ -0,0 +1,69 @@
+package models
+
+import play.api.libs.json.{JsString, JsObject}
+import play.api.test._
+import play.api.test.Helpers._
+import resources._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class VungleCallbackSpec extends SpecificationWithFixtures with AdProviderSpecSetup with WaterfallSpecSetup {
+  running(FakeApplication(additionalConfiguration = testDB)) {
+    val id = WaterfallAdProvider.create(waterfall.id, vungleID, None, None, true, true).get
+    val currentWap = WaterfallAdProvider.find(id).get
+    val configuration = JsObject(Seq("callbackParams" -> JsObject(Seq("APIKey" -> JsString("abcdefg"))),
+      "requiredParams" -> JsObject(Seq()), "reportingParams" -> JsObject(Seq())))
+    WaterfallAdProvider.update(new WaterfallAdProvider(currentWap.id, currentWap.waterfallID, currentWap.adProviderID, None, None, Some(true), None, configuration, false))
+  }
+
+  val transactionID = "0123456789"
+  val digest = "bf80d53f84df22bb91b48acc7606bc0909876f6fe981b1610a0352433ae16a63"
+  val amount = 1
+  val callback = running(FakeApplication(additionalConfiguration = testDB)) {
+    new VungleCallback(app1.token, transactionID, digest, amount)
+  }
+
+  "adProviderName" should {
+    "be set when creating a new instance of the VungleCallback class" in new WithDB {
+      callback.adProviderName must beEqualTo("Vungle")
+    }
+  }
+
+  "token" should {
+    "be set when creating a new instance of the VungleCallback class" in new WithDB {
+      callback.token must beEqualTo(app1.token)
+    }
+  }
+
+  "currencyAmount" should {
+    "be set when creating a new instance of the VungleCallback class" in new WithDB {
+      callback.currencyAmount must beEqualTo(amount)
+    }
+  }
+
+  "receivedVerification" should {
+    "be set when creating a new instance of the VungleCallback class" in new WithDB {
+      callback.receivedVerification must beEqualTo(digest)
+    }
+  }
+
+  "verificationInfo" should {
+    "return an instance of the CallbackVerificationInfo class" in new WithDB {
+      callback.verificationInfo must haveClass[CallbackVerificationInfo]
+    }
+
+    "be valid when the generated verification matches the received verification string" in new WithDB {
+      val verification = callback.verificationInfo
+      verification.isValid must beEqualTo(true)
+    }
+
+    "not be valid when the generated verification does not match the received digest" in new WithDB {
+      val invalidDigest = "Some fake verifier"
+      val newCallback = new VungleCallback(app1.token, transactionID, invalidDigest, amount)
+      val verification = newCallback.verificationInfo
+      verification.isValid must beEqualTo(false)
+    }
+  }
+  step(clean)
+}
