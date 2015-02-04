bcabf764968a5190ae867ff13676ff66b54307a5
David Cullen <dcullen@jungroup.com>
2015-02-04 17:11:14 -0500

    [MED-469] Fix tests post angular

--- test/functional/DistributorUsersControllerSpec.scala
+++ test/functional/DistributorUsersControllerSpec.scala
@@ -7,7 +7,7 @@ import play.api.test.Helpers._
 class DistributorUsersControllerSpec extends SpecificationWithFixtures {
   "DistributorUsersController.signup" should {
     "disable the submit button if terms are not agreed to" in new WithFakeBrowser {
-      browser.goTo(controllers.routes.DistributorUsersController.signup.url)
+      goToAndWaitForAngular(controllers.routes.DistributorUsersController.signup.url)
       browser.fill("#company").`with`(companyName)
       browser.fill("#email").`with`(email)
       browser.fill("#password").`with`(password)
@@ -16,7 +16,7 @@ class DistributorUsersControllerSpec extends SpecificationWithFixtures {
     }
 
     "hide the license agreement unless the user clicks on the License Agreement link" in new WithFakeBrowser {
-      browser.goTo(controllers.routes.DistributorUsersController.signup.url)
+      goToAndWaitForAngular(controllers.routes.DistributorUsersController.signup.url)
       browser.find("#termsContainer").first().isDisplayed must beEqualTo(false)
       browser.$("#viewTerms").click()
       browser.find("#termsContainer").first().isDisplayed must beEqualTo(true)
@@ -26,7 +26,7 @@ class DistributorUsersControllerSpec extends SpecificationWithFixtures {
   "DistributorUsersController.create" should {
     "if the user's email is taken, render the sign up page with all fields filled except the password fields" in new WithFakeBrowser {
       DistributorUser.create(email, password, companyName)
-      browser.goTo(controllers.routes.DistributorUsersController.signup.url)
+      goToAndWaitForAngular(controllers.routes.DistributorUsersController.signup.url)
       browser.fill("#company").`with`(companyName)
       browser.fill("#email").`with`(email)
       browser.fill("#password").`with`(password)
@@ -59,7 +59,7 @@ class DistributorUsersControllerSpec extends SpecificationWithFixtures {
       val user = DistributorUser.findByEmail(email).get
       logInUser()
       browser.goTo(controllers.routes.DistributorUsersController.logout.url)
-      browser.goTo(controllers.routes.AnalyticsController.show(user.distributorID.get, None).url)
+      goToAndWaitForAngular(controllers.routes.AnalyticsController.show(user.distributorID.get, None).url)
       browser.pageSource must contain("Welcome Back")
     }
   }
