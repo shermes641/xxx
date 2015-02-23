ce5268d769c3a76423c04fa870546872924bc7eb
David Cullen <dcullen@jungroup.com>
2015-02-23 11:27:34 -0500

    [MED-207] CSV debugging v2

--- app/models/Mailer.scala
+++ app/models/Mailer.scala
@@ -25,8 +25,10 @@ trait Mailer {
       mail.setSubject(subject)
       if(attachmentFileName != "") {
         val format = new SimpleDateFormat("d-M-y")
+        println("Email Attachment: " + format.format(Calendar.getInstance().getTime()) + "-export.csv")
         mail.addAttachment(format.format(Calendar.getInstance().getTime()) + "-export.csv", new File(attachmentFileName))
       }
+      println(mail)
       val template = views.html.Mails.emailTemplate(subject, body, host).toString()
       mail.sendHtml(template)
     }
