6e46320cc70eb84640059102e677ce627ce318a1
David Cullen <dcullen@jungroup.com>
2015-03-09 16:47:14 -0400

    [MED-71] Plain text email feedback.

--- app/models/WelcomeEmailActor.scala
+++ app/models/WelcomeEmailActor.scala
@@ -26,12 +26,12 @@ class WelcomeEmailActor extends Actor with Mailer {
     val subject = "Welcome to HyprMediate!"
 
     val body = views.html.Mails.welcomeEmailContent().toString()
-    val plain: String = { "Thank you for registering with HyprMediate.\r\n\r\n " +
+    val plain: String = { "Thank you for registering with HyprMediate.\r\n\r\n" +
       "HyprMediate is the easiest way to manage monetization partners across all of your applications." +
-      " Our platform is simple to use, and the setup is quick and seamless.\r\n\r\n  " +
-      "To get started, please visit our documentation.  http://documentation.hyprmx.com\r\n\r\n " +
-      "If you have any questions, feel free to contact us by replying to this email.\r\n\r\n " +
-      "We're looking forward to working with you!\r\n\r\n Sincerely,\r\nYour HyprMediate\r\nPublisher Accounts Team" }
+      "Our platform is simple to use, and the setup is quick and seamless.\r\n\r\n " +
+      "To get started, please visit our documentation.  http://documentation.hyprmx.com\r\n\r\n" +
+      "If you have any questions, feel free to contact us by replying to this email.\r\n\r\n" +
+      "We're looking forward to working with you!\r\n\r\nSincerely,\r\nYour HyprMediate\r\nPublisher Accounts Team" }
 
     sendEmail(email, subject, body, plain)
   }
