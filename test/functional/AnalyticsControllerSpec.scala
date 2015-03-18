7c6bf58a2ec8c86f5d47c354982ba829e870e59a
David Cullen <dcullen@jungroup.com>
2015-03-18 12:47:47 -0400

    [MED-427] PR feedback, view source of test

--- test/functional/AnalyticsControllerSpec.scala
+++ test/functional/AnalyticsControllerSpec.scala
@@ -196,7 +196,7 @@ class AnalyticsControllerSpec extends SpecificationWithFixtures with Distributor
 
     "Pass Jasmine tests" in new WithAppBrowser(distributorUser.distributorID.get) {
       browser.goTo(routes.Assets.at("/javascripts/test/SpecRunner.html").url)
-      browser.await().atMost(5, java.util.concurrent.TimeUnit.SECONDS).until(".bar.passed").containsText("0 failures")
+      browser.pageSource must contain("0 failures")
     }
   }
 }
