32ad4a685230c15d82d88c37f301c22cb5d22a81
David Cullen <dcullen@jungroup.com>
2015-06-09 11:03:19 -0400

    [MED-1024] Disable ability to submit twice when creating new app for first time.  New tests for new app creation.

--- public/javascripts/test/spec/NewAppSpec.js
+++ public/javascripts/test/spec/NewAppSpec.js
@@ -2,7 +2,7 @@ describe('NewAppSpec', function() {
     beforeEach(module('MediationModule'));
 
 
-    describe('CreateNewApp', function() {
+    describe('CreateNewApp - Waterfall', function() {
         var scope, testCont, compile, form, newAppResponseMock;
 
         beforeEach(inject(function($rootScope, $controller, $compile, $httpBackend) {
@@ -118,6 +118,115 @@ describe('NewAppSpec', function() {
 
             newAppResponseMock.respond(400, { status:"error", fieldName:"appName", message:"You already have an App with the same name.  Please choose a unique name for your new App." });
             httpBackend.flush();
+
+            form.$valid = true;
+            scope.submitNewApp(form);
+            httpBackend.flush();
+            expect(scope.errors.appName).toEqual("You already have an App with the same name.  Please choose a unique name for your new App.");
+            expect(scope.errors.appNameClass).toEqual("error");
+        });
+    });
+
+    describe('CreateNewApp - First App', function() {
+        var scope, testCont, compile, form, newAppResponseMock;
+
+        beforeEach(inject(function($rootScope, $controller, $compile, $httpBackend) {
+            scope = $rootScope.$new();
+            httpBackend = $httpBackend;
+
+            // Mock new app response
+            newAppResponseMock = httpBackend.when("POST", "/distributors/456/apps");
+
+            element = angular.element(
+                '<form name="form.newAppForm">' +
+                    '<input required type="text" ng-model="newApp.appName" ng-model-options="{updateOn: \'blur\'}" id="newAppName" name="appName" placeholder="name" label="*App Name">' +
+                    '<input required-integer type="text" ng-model="newApp.exchangeRate" ng-model-options="{updateOn: \'blur\'}" id="newAppExchangeRate" name="exchangeRate" placeholder="e.g. 100">' +
+                    '<input type="text" ng-model="newApp.rewardMin" ng-model-options="{updateOn: \'blur\'}" id="newAppRewardMin" name="rewardMin">' +
+                    '<input greater-than-or-equal-to="{{data.rewardMin}}" type="text" ng-model="newApp.rewardMax" ng-model-options="{updateOn: \'blur\'}" id="newAppRewardMax" name="rewardMax">' +
+                    '</form>'
+            );
+
+            scope.newApp = {rewardMax: undefined, rewardMin: undefined, exchangeRate: undefined, appName: undefined};
+            testCont = $controller('NewAppsController', {$scope: scope, $routeParams: { distributorID: 456 }});
+
+            compile = $compile;
+            compile(element)(scope);
+            form = scope.form.newAppForm;
+            scope.testing = true;
+        }));
+
+        it('should have the waterfall controller defined', function() {
+            expect(testCont).toBeDefined();
+        });
+
+        it('should be initalized correctly', function() {
+            expect(scope.showNewAppModal).toEqual(undefined);
+            expect(scope.newAppModalTitle).toEqual("Welcome to hyprMediate!");
+        });
+
+        it('should not be able to submit twice until we receive a response from the server (200)', function() {
+            scope.newApp.rewardMax = "123";
+            scope.newApp.rewardMin = "13";
+            scope.newApp.exchangeRate = "1";
+            scope.$digest();
+
+            newAppResponseMock.respond(200, { data: "" })
+
+            scope.submitNewApp(form);
+            expect(form.submitting).toEqual(undefined);
+
+            form.$valid = true;
+            scope.submitNewApp(form);
+            // Before new app response, form should be in submitting state
+            expect(form.submitting).toEqual(true);
+            httpBackend.flush();
+            // Only after request has completed should you be able to submit again
+            expect(form.submitting).toEqual(false);
+        });
+
+        it('should not be able to submit twice until we receive a response from the server (400)', function() {
+            scope.newApp.rewardMax = "123";
+            scope.newApp.rewardMin = "13";
+            scope.newApp.exchangeRate = "1";
+            scope.newApp.appName = "test";
+            scope.$digest();
+
+            newAppResponseMock.respond(400, { data: "" });
+
+            form.$valid = true;
+            scope.submitNewApp(form);
+            // Before new app response, form should be in submitting state
+            expect(form.submitting).toEqual(true);
+            httpBackend.flush();
+            // Only after request has completed should you be able to submit again
+            expect(form.submitting).toEqual(false);
+        });
+
+        it('should be able to submit again after response is 500', function() {
+            scope.newApp.rewardMax = "123";
+            scope.newApp.rewardMin = "13";
+            scope.newApp.exchangeRate = "1";
+            scope.$digest();
+
+            newAppResponseMock.respond(500, { data: "" });
+
+            form.$valid = true;
+            scope.submitNewApp(form);
+            // Before new app response, form should be in submitting state
+            expect(form.submitting).toEqual(true);
+            httpBackend.flush();
+            // Only after request has completed should you be able to submit again
+            expect(form.submitting).toEqual(false);
+        });
+
+        it('should handle error if server returns 400', function(){
+            scope.newApp.rewardMax = "123";
+            scope.newApp.rewardMin = "13";
+            scope.newApp.exchangeRate = "1";
+            scope.newApp.appName = "test";
+            scope.$digest();
+
+            newAppResponseMock.respond(400, { status:"error", fieldName:"appName", message:"You already have an App with the same name.  Please choose a unique name for your new App." });
 
             form.$valid = true;
             scope.submitNewApp(form);
