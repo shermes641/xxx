4d8cfa93ca1edd1c602d771e89ccffa8d43ec591
JULIAN SALAMA <JSALAMA@JUNGROUP.COM>
2015-08-13 15:18:49 -0400

    [MED-1483] forgot to include driver

--- test/resources/SpecificationWithFixtures.scala
+++ test/resources/SpecificationWithFixtures.scala
@@ -21,7 +21,7 @@ abstract class SpecificationWithFixtures extends Specification with CleanDB with
     Play.current.configuration.getString("httpAuthPassword").getOrElse("")
   }
 
-  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver_linux");
 
   /**
    * Drops and recreates database schema after tests are run.
