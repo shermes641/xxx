cc3f04a5cb7480ad288c950be0c11209bebf6beb
Tom Depplito <tdepplito@jungroup.com>
2016-03-02 16:42:09 -0500

    [MED-2074] Remove Vungle for Android

--- test/models/PlatformSpec.scala
+++ test/models/PlatformSpec.scala
@@ -1,6 +1,19 @@
 package models
 
+import models.Platform.{Android, Ios}
+import play.api.test._
+import play.api.test.Helpers._
+
 class PlatformSpec extends SpecificationWithFixtures {
+  val adProviderNames = running(FakeApplication(additionalConfiguration = testDB)) {
+    List(
+      Ios.AdColony.name,
+      Ios.HyprMarketplace.name,
+      Ios.Vungle.name,
+      Ios.AppLovin.name
+    )
+  }
+
   "Platform.find" should {
     "return the correct platform for a given ID" in new WithDB {
       Platform.find(Platform.Android.PlatformID).PlatformName must beEqualTo(Platform.Android.PlatformName)
@@ -11,5 +24,31 @@ class PlatformSpec extends SpecificationWithFixtures {
       val unknownID = 0
       Platform.find(unknownID).PlatformName must beEqualTo(Platform.Ios.PlatformName)
     }
+  }
+
+  "Ios.allAdProviders" should {
+    "return the 4 supported ad networks" in new WithDB {
+      val allAdProviders = Ios.allAdProviders
+      verifyAdProviders(allAdProviders, adProviderNames)
+    }
+  }
+
+  "Android.allAdProviders" should {
+    "return the 3 supported ad networks" in new WithDB {
+      val adProviderNamesWithoutVungle = adProviderNames.filter(_ != Ios.Vungle.name)
+      val allAdProviders = Android.allAdProviders
+      verifyAdProviders(allAdProviders, adProviderNamesWithoutVungle)
+    }
+
+    "not include Vungle" in new WithDB {
+      Android.allAdProviders.count(_.name == Ios.Vungle.name) must beEqualTo(0)
+    }
+  }
+
+  def verifyAdProviders(allAdProviders: List[UpdatableAdProvider], adProviderNames: List[String]) = {
+    allAdProviders.length must beEqualTo(adProviderNames.length)
+    adProviderNames.map(adProviderName =>
+      allAdProviders.count(_.name == adProviderName) must beEqualTo(1)
+    )
   }
 }
